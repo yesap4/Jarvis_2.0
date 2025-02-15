bc3279846e8870e92c3529ee86e5d26b0b8ed159
Anant Prakash <yes4ap@gmail.com>
2025-02-15 04:15:35 +0000

    Updated

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-def speak ()
+import pyttsx3
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[0].id)
+engine.getProperty('voice', voices[0].id)
+
+
+def speak (audio):
+    pass
