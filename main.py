86df504c004088da6825c4b100f91c9e9efe87b8
Anant Prakash <yes4ap@gmail.com>
2025-02-15 08:15:27 +0000

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pyttsx3
 
 
-engine = pyttsx3.init('sapi5')
+engine = pyttsx3.init('espeak')
 voices = engine.getProperty('voices')
 print(voices[1].id)
 engine.getProperty('voice', voices[0].id)
